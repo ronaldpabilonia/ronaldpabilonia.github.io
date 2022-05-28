1d1e1e2361ba011483808c93670684f9257dca1d
DESKTOP-CC7PDB0\Ronald <Ronald.Parker@firstmac.com.au>
2022-05-29 02:24:15 +0800

    added pdf downloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import domtoimage from 'dom-to-image';
+import jspdf from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cv';
+  downloading = false;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  download () {
+    let img: any;
+    let filename: any;
+    let newImage: any;
+    this.downloading = true;
+
+    setTimeout(() => {
+      const node: any = this.document.getElementById('body');
+      domtoimage
+      .toPng(node, { bgcolor: '#fff' })
+      .then((dataUrl) => {
+        img = new Image();
+        img.src = dataUrl;
+        newImage = img.src;
+
+        img.onload = () => {
+          let doc;
+          let pdfWidth = img.width;
+          let pdfHeight = img.height;
+
+          if (pdfWidth > pdfHeight) {
+            doc = new jspdf('l', 'px', [pdfWidth , pdfHeight]);
+          }
+          else {
+            doc = new jspdf('p', 'px', [pdfWidth , pdfHeight]);
+          }
+
+          const width = doc.internal.pageSize.getWidth();
+          const height = doc.internal.pageSize.getHeight();
+
+          doc.addImage(newImage, 'PNG',  10, 10, width, height);
+          filename = 'pabilonia_ronald_cv' + '.pdf';
+          doc.save(filename);
+          this.downloading = false;
+        };
+      })
+      .catch((error) => {
+        this.downloading = false;
+      });
+    }, 500);
+  }
 }
